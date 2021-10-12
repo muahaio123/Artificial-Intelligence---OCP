b93f87d7726c83cc48e893478a257ab3513b1c63
muahaio123 <69134357+muahaio123@users.noreply.github.com>
2021-10-12 16:47:12 -0500

    Create ocpAgentFour.cpp

---
+++ ocpAgentFour.cpp
@@ -0,0 +1,32 @@
+// YOUR NAME: Nam Bui
+//
+// ASU AI Competition
+// Agent Challenge A: One-card poker
+//
+// Rename this file and the function below.  For example, if your agent name
+// is Jones, rename this ocpAgentSmith.cpp file to ocpAgentJones.cpp and the
+// ocpAgentSmith function below to ocpAgentJones.  Complete your agent
+// function and turn it in on Blackboard.  Feel free to create other
+// agents--each in a separate .cpp file--for yours to compete against, but
+// turn in only one.  Test your agent(s) with
+//
+//    nice bash ocpBuild.bash
+//
+// and then
+//
+//    nice ./ocpRunSim
+//
+// Each submitted agent will play each other in matchups of some number of
+// hands each (at least 100) to determine the standings, which will be
+// posted soon after the agents are due.
+
+#include "ocp.h"
+
+namespace
+{
+}
+
+// Rename and complete this agent function.
+
+int ocpAgentFour(const MatchupHistory &mh)
+{	return 4;	}
